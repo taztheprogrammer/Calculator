cf17315222ca67388413f85c44ce26eb494f9a13
taztheprogrammer <123741432+taztheprogrammer@users.noreply.github.com>
2023-09-11 21:30:28 -0400

    Added + - functionality
    Can now change display from current to previous

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 const current = document.getElementById('current');
+const pre = document.getElementById("pre");
 
 const btn = document.getElementsByClassName('btn');
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
 
 for (let i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', displayChange);
 }
 
-function displayChange() {
-    current.innerHTML = this.innerHTML;
+function switchDisplay() {
+    pre.innerHTML = current.innerHTML;
+    current.innerHTML = ""
 }
+
+function Add() {
+    switchDisplay();
+}
+
+function Minus() {
+    switchDisplay();
+}
+
+function displayChange() {
+    if (this.innerHTML === '+') {
+        add();
+    } else {
+        const temp = current.innerHTML;
+        current.innerHTML = temp + this.innerHTML;
+    }
+}
+
+
+add.addEventListener('click', Add);
+minus.addEventListener('click', Minus)
